2cac1a62730bdd9d55861e1130884d39c6688f3c
Nikita Bolotin <ridge96@yandex.ru>
2022-11-24 05:39:41 +0300

    implement waiting for mined and rendering nonce
    Signed-off-by: Nikita Bolotin <ridge96@yandex.ru>

--- src/app/components/wallet-integration/wallet-integration.component.ts
+++ src/app/components/wallet-integration/wallet-integration.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { WalletIntegrationService } from '../../services/wallet-integration/wallet-integration.service';
 import { NgIf } from '@angular/common';
+import { ethers } from 'ethers';
 
 @Component({
     standalone: true,
@@ -15,6 +16,7 @@ import { NgIf } from '@angular/common';
 export class WalletIntegrationComponent implements OnInit {
     public explorerUrl = 'https://goerli.etherscan.io/tx/';
     public txHash = '';
+    public nonce: number | undefined;
 
     constructor(private walletIntegrationService: WalletIntegrationService,
                 private cdr: ChangeDetectorRef) {
@@ -28,6 +30,12 @@ export class WalletIntegrationComponent implements OnInit {
                     .then((state) => {
                         this.txHash = state.hash;
                         this.cdr.detectChanges();
+                        this.walletIntegrationService.listenIsTransactionMined(this.txHash,
+                            () => this.walletIntegrationService.getNonce(this.txHash)
+                                .then((txResponse: ethers.providers.TransactionResponse) => {
+                                    this.nonce = txResponse.nonce;
+                                    this.cdr.detectChanges();
+                                }));
                     });
             });
     }
--- src/app/services/wallet-integration/wallet-integration.service.ts
+++ src/app/services/wallet-integration/wallet-integration.service.ts
@@ -5,38 +5,33 @@ import { Contract, ethers } from 'ethers';
     providedIn: 'root'
 })
 export class WalletIntegrationService {
-    private _provider: ethers.providers.Web3Provider = {} as ethers.providers.Web3Provider;
-    private _signer: ethers.providers.JsonRpcSigner = {} as ethers.providers.JsonRpcSigner;
-    private _erc20: Contract = {} as Contract;
+    private provider: ethers.providers.Web3Provider = {} as ethers.providers.Web3Provider;
+    private signer: ethers.providers.JsonRpcSigner = {} as ethers.providers.JsonRpcSigner;
+    private erc20: Contract = {} as Contract;
 
     private readonly address = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6';
     private readonly abi = ['function approve(address _spender, uint256 _value) public returns (any state)'];
 
-    public get provider(): ethers.providers.Web3Provider {
-        return this._provider;
-    }
-
-    public get signer(): ethers.providers.JsonRpcSigner {
-        return this._signer;
-    }
-
-    public get erc20(): Contract {
-        return this._erc20;
-    }
-
     public async initMetaMaskConnection(): Promise<void> {
         // @ts-ignore
-        this._provider = new ethers.providers.Web3Provider(window.ethereum);
-        await this._provider.send('eth_requestAccounts', []);
-        this._signer = this._provider.getSigner();
+        this.provider = new ethers.providers.Web3Provider(window.ethereum);
+        await this.provider.send('eth_requestAccounts', []);
+        this.signer = this.provider.getSigner();
     }
 
     public connectToContract(): void {
-        this._erc20 = new ethers.Contract(this.address, this.abi, this._signer);
+        this.erc20 = new ethers.Contract(this.address, this.abi, this.signer);
     }
-
 
     public async approveContract(): Promise<any> {
         return await this.erc20['approve'](this.address, ethers.utils.parseUnits('1.0', 'wei'));
+    }
+
+    public listenIsTransactionMined(txHash: string, listener: () => void): void {
+        this.provider.once(txHash, listener);
+    }
+
+    public async getNonce(txHash: string): Promise<ethers.providers.TransactionResponse> {
+        return await this.provider.getTransaction(txHash);
     }
 }
